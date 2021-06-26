570143484a407fa0c7714de74af2fe8769e0e6a0
Abhirup5Dutta <abhirupdutta04acse@gmail.com>
2021-06-26 20:14:16 +0530

    Mobile view making 2nd commit

--- src/PageBodyLeft/PageHeaderLeft/PageHeaderLeft.js
+++ src/PageBodyLeft/PageHeaderLeft/PageHeaderLeft.js
@@ -5,6 +5,9 @@ import PageRightHeader from '../../PageBodyRight/PageRightHeader/PageRightHeader
 function PageHeaderLeft() {
     return (
         <div className="pageMob">
+            <div className='logo'>
+                <img src="https://cdn.iconscout.com/icon/free/png-512/shazam-1-722737.png" alt="Company Logo" />
+            </div>
             <div className="pageHeader__down__left">
                 <h3>Dashboard</h3>
                 <h5>Monday, <span className="pageHeader__date">02 July 2020</span></h5>
